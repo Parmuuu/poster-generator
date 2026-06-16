f2ccdc6196139a477db880d065d6ba2209d6bda0
Parmuuu <130922799+Parmuuu@users.noreply.github.com>
2026-06-16 11:54:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -33,8 +33,8 @@ function generatePoster() {
                 ctx.beginPath();
 
                 ctx.arc(
-                    300,
-                    800,
+                    305,
+                    805,
                     260,
                     0,
                     Math.PI * 2
@@ -45,8 +45,8 @@ function generatePoster() {
 
                 ctx.drawImage(
                     userImg,
-                    -10,
-                    430,
+                    -5,
+                    435,
                     780,
                     780
                 );
