7402940487a0230f1a0554479708d6314c532932
Parmuuu <130922799+Parmuuu@users.noreply.github.com>
2026-06-16 10:35:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,76 @@
 function generatePoster() {
-    alert("WORKING");
+
+    const name = document.getElementById("name").value;
+    const file = document.getElementById("photo").files[0];
+
+    if (!file) {
+        alert("Please upload a photo");
+        return;
+    }
+
+    const canvas = document.getElementById("canvas");
+    const ctx = canvas.getContext("2d");
+
+    const poster = new Image();
+
+    poster.onload = function () {
+
+        canvas.width = poster.width;
+        canvas.height = poster.height;
+
+        ctx.drawImage(poster, 0, 0);
+
+        const reader = new FileReader();
+
+        reader.onload = function (e) {
+
+            const userImg = new Image();
+
+            userImg.onload = function () {
+
+                ctx.save();
+
+                ctx.beginPath();
+
+                ctx.arc(165, 265, 125, 0, Math.PI * 2);
+
+                ctx.closePath();
+                ctx.clip();
+
+                ctx.drawImage(
+                    userImg,
+                    40,
+                    140,
+                    250,
+                    250
+                );
+
+                ctx.restore();
+
+                let fontSize = 42;
+
+                if (name.length > 20) fontSize = 32;
+                if (name.length > 30) fontSize = 26;
+
+                ctx.fillStyle = "#7c2d6a";
+                ctx.textAlign = "center";
+                ctx.font = `bold ${fontSize}px Arial`;
+
+                ctx.fillText(
+                    name,
+                    canvas.width / 2,
+                    poster.height - 180
+                );
+
+            };
+
+            userImg.src = e.target.result;
+
+        };
+
+        reader.readAsDataURL(file);
+
+    };
+
+    poster.src = "poster.png";
 }
